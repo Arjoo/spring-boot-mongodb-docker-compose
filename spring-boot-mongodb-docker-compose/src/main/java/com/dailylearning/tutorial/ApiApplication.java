e80b8e6dd6964568be06d45bb00e26cb7e7e731a
arjoo <2009arjoo@gmail.com>
2024-02-15 13:07:13 +0530

    added changes in properties file

--- spring-boot-mongodb-docker-compose/src/main/java/com/dailylearning/tutorial/ApiApplication.java
+++ spring-boot-mongodb-docker-compose/src/main/java/com/dailylearning/tutorial/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.dailylearning.tutorial;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiApplication {
+
+	@Value("${app.ui.host.cors.uri}")
+	private String uiHostUrl;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
@@ -18,7 +22,7 @@ public class ApiApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/*").allowedOrigins(uiHostUrl);
 			}
 		};
 	}
